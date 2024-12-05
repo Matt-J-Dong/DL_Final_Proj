f35327940a3b4dc89ad6dafd2786ba281b16d460
dq2024 <dq2024@nyu.edu>
2024-12-05 17:22:54 -0500

    changes

--- dataset.py
+++ dataset.py
@@ -58,7 +58,8 @@ def create_wall_dataloader(
         batch_size,
         shuffle=train,
         drop_last=True,
-        pin_memory=False,
+        pin_memory=True,
+        num_workers=4
     )
 
     return loader
