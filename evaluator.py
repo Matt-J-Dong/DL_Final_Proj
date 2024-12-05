5851c79eaec35472342f3ca1d93bd103e93dd078
dq2024 <dq2024@nyu.edu>
2024-12-05 12:23:55 -0500

    formatting changes

--- evaluator.py
+++ evaluator.py
@@ -115,18 +115,9 @@ class ProbingEvaluator:
             for batch in tqdm(dataset, desc="Probe prediction step"):
                 ################################################################################
                 # TODO: Forward pass through your model
-                # # EDITED
-                # init_states = batch.states[:, :-1]  # [BS, T-1, C, H, W]
-                # actions = batch.actions  # [BS, T-1, action_dim]
-
-                # pred_encs = model(states=init_states, actions=actions)
-                # pred_encs = pred_encs.transpose(0, 1)  # [T-1, BS, D]
-
                 # EDITED
                 init_state = batch.states[:, 0]  # [B, C, H, W]
                 actions = batch.actions  # [B, T-1, action_dim]
-
-                # Forward pass through the model
                 pred_encs = model(init_state, actions)  # [B, T, D]
                 pred_encs = pred_encs.transpose(0, 1)
 
@@ -227,16 +218,10 @@ class ProbingEvaluator:
         for idx, batch in enumerate(tqdm(val_ds, desc="Eval probe pred")):
             ################################################################################
             # TODO: Forward pass through your model
-            # init_states = batch.states[:, 0:1]  # BS, 1 C, H, W
-            # pred_encs = model(states=init_states, actions=batch.actions)
-            # # # BS, T, D --> T, BS, D
-            # pred_encs = pred_encs.transpose(0, 1)
 
             #EDITED
             init_state = batch.states[:, 0]  # [B, C, H, W]
             actions = batch.actions  # [B, T-1, action_dim]
-
-            # Forward pass through the model
             pred_encs = model(init_state, actions)  # [B, T, D]
             pred_encs = pred_encs.transpose(0, 1)  # [B, T, D] --> [T, B, D]
 
