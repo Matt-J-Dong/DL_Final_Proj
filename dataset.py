a3bbd56e4c091f13e11dd46f6187eac0a118c77c
dq2024 <dq2024@nyu.edu>
2024-12-06 12:20:06 -0500

    .

--- dataset.py
+++ dataset.py
@@ -62,7 +62,7 @@ def create_wall_dataloader(
         batch_size,
         shuffle=train,
         drop_last=True,
-        pin_memory=True,
+        pin_memory=False,
         num_workers=0
     )
 
