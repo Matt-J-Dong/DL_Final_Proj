57f0301f4508c7f5576776dc309e394205679387
jackziyangchen <jackczy1209@gmail.com>
2024-12-12 19:41:01 -0500

    nvm

--- jc_train_1_gpu_v3.py
+++ jc_train_1_gpu_v3.py
@@ -95,7 +95,7 @@ class Trainer:
                 )
                 epoch_loss += loss
 
-                if batch_idx % 7 == 0:
+                if batch_idx % 10 == 0:
                     wandb.log({"loss": loss, 
                                "energy_loss": e_loss, 
                                "variance_loss": var_loss, 
