df9bd9266e36aa9a92e02efb701afa9f367e1055
jackziyangchen <jackczy1209@gmail.com>
2024-12-13 17:14:27 -0500

    hyp tune

--- jc_train_1_gpu_v3.py
+++ jc_train_1_gpu_v3.py
@@ -129,12 +129,12 @@ def main():
         "momentum": 0.996,
         "split_ratio": 0.9,
         "lambda_energy": 1.0,
-        "lambda_var": 0.1,
+        "lambda_var": 1.0,
         "lambda_cov": 0.01,
         "max_grad_norm": 1.0,
         "min_variance": 1.0,
         "save_every": 1,
-        'margin': 1.0,
+        'margin': 0.5,
         'lambda_contrastive': 0.05,
     }
 
