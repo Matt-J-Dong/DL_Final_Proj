f355df650d69298edc76919fdbf5def4906ce18e
Matt-J-Dong <matthew.dong@nyu.edu>
2024-12-14 17:42:53 -0500

    batch size

--- md_train_v2.py
+++ md_train_v2.py
@@ -273,7 +273,7 @@ class Trainer:
 
 def main():
     config = {
-        "batch_size": 1024,
+        "batch_size": 256,
         "num_epochs": 4,
         "learning_rate": 5e-5,
         'step_per_epoch': 1000,
