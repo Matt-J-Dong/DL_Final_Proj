0c2fa1926b8948d03c8db1ea2be72cd194e3c89a
Matt-J-Dong <matthew.dong@nyu.edu>
2024-12-14 18:40:34 -0500

    v2

---
+++ main_md_v2.py
@@ -0,0 +1,89 @@
+from dataset import create_wall_dataloader
+from evaluator import ProbingEvaluator
+import torch
+from models_md_1 import JEPA_Model
+import glob
+
+
+def get_device():
+    """Check for GPU availability."""
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+    return device
+
+
+def load_data(device):
+    data_path = "/scratch/DL24FA"
+
+    probe_train_ds = create_wall_dataloader(
+        data_path=f"{data_path}/probe_normal/train",
+        probing=True,
+        device=device,
+        train=True,
+    )
+
+    probe_val_normal_ds = create_wall_dataloader(
+        data_path=f"{data_path}/probe_normal/val",
+        probing=True,
+        device=device,
+        train=False,
+    )
+
+    probe_val_wall_ds = create_wall_dataloader(
+        data_path=f"{data_path}/probe_wall/val",
+        probing=True,
+        device=device,
+        train=False,
+    )
+
+    probe_val_ds = {"normal": probe_val_normal_ds, "wall": probe_val_wall_ds}
+
+    return probe_train_ds, probe_val_ds
+
+
+def load_model():
+    """Load or initialize the model."""
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # TODO: Replace MockModel with your trained model
+    model = JEPA_Model()
+    # Load the saved model weights
+    #state_dict = torch.load('./checkpoints/jepa_model_epoch_final.pth', map_location=torch.device(device))
+
+    state_dict = torch.load('./checkpoints/jepa_model_v2_epoch_final.pth', map_location=torch.device(device))
+
+    #state_dict = torch.load('./checkpoints/jepa_model_epoch_10.pth', map_location=torch.device('cpu'))
+    
+    # Handle potential 'module.' prefix in state_dict keys
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:] if k.startswith('module.') else k  # remove 'module.' prefix if present
+        new_state_dict[name] = v
+
+    model.load_state_dict(new_state_dict)
+    model.to(device)
+    return model
+
+
+def evaluate_model(device, model, probe_train_ds, probe_val_ds):
+    evaluator = ProbingEvaluator(
+        device=device,
+        model=model,
+        probe_train_ds=probe_train_ds,
+        probe_val_ds=probe_val_ds,
+        quick_debug=False,
+    )
+
+    prober = evaluator.train_pred_prober()
+
+    avg_losses = evaluator.evaluate_all(prober=prober)
+
+    for probe_attr, loss in avg_losses.items():
+        print(f"{probe_attr} loss: {loss}")
+
+
+if __name__ == "__main__":
+    device = get_device()
+    probe_train_ds, probe_val_ds = load_data(device)
+    model = load_model()
+    evaluate_model(device, model, probe_train_ds, probe_val_ds)
