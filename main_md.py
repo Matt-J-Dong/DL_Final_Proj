4289354da46b47018ae97380e987b295a7b6fbcc
Matt-J-Dong <matthew.dong@nyu.edu>
2024-12-14 14:14:15 -0500

    testing

--- main_md.py
+++ main_md.py
@@ -46,8 +46,8 @@ def load_model():
     # TODO: Replace MockModel with your trained model
     model = JEPA_Model()
     # Load the saved model weights
-    #state_dict = torch.load('./checkpoints/jepa_model_epoch_final.pth', map_location=torch.device('cpu'))
-    state_dict = torch.load('./checkpoints/jepa_model_epoch_10.pth', map_location=torch.device('cpu'))
+    state_dict = torch.load('./checkpoints/jepa_model_testing_epoch_final.pth', map_location=torch.device('cpu'))
+    #state_dict = torch.load('./checkpoints/jepa_model_epoch_10.pth', map_location=torch.device('cpu'))
     
     # Handle potential 'module.' prefix in state_dict keys
     from collections import OrderedDict
