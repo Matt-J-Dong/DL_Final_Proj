2cebdcdccec9bd98177a3e5c00871cb11dfa722d
dq2024 <dq2024@nyu.edu>
2024-12-15 16:26:12 -0500

    trying byol model

--- dq_train_1_gpu_v4.py
+++ dq_train_1_gpu_v4.py
@@ -100,19 +100,20 @@ def main():
     config = {
         "batch_size": 512,
         "num_epochs": 20,
-        "learning_rate": 1e-5,
+        "learning_rate": 2e-4,
         # 'step_per_epoch': 1000,
         "momentum": 0.996,
         "split_ratio": 1.0,
-        "lambda_energy": 1.0,
+        "lambda_energy": 25.0,
         "lambda_var": 25.0,
-        "lambda_cov": 1.0,
-        'lambda_contrastive': 1.0,
+        "lambda_cov": 5.0,
+        'lambda_contrastive': 0.1,
         "max_grad_norm": 1.0,
         "min_variance": 1.0,
         "save_every": 1,
         'margin': 0.5,
         'distance_function': 'l2',
+        'repr_dim': 128,
     }
 
 
